8b8452b2ba6e8422114fa86c86f2637161558017
ansunyan <ansun00@gmail.com>
2023-04-11 21:57:20 -0600

    Parkinson Canada project

--- js/gallery-tags.js
+++ js/gallery-tags.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#portfolio',
     data: {
         items: [
+		{ id: 'parkinson-canada', format: 'jpg', tags: ['all', 'web', 'branding'], alt: 'Parkinson Canada brand and website evolution' },
             { id: 'vrcave-web', format: 'jpg', tags: ['all', 'web', 'video', 'branding','ux'], alt: 'vrCAVE - Website Redesign' },
             { id: 'arnet-web', format: 'jpg', tags: ['all', 'web', 'branding','ux'], alt: 'ARNET - Website Redesign' },
             { id: 'alignvr-web', format: 'jpg', tags: ['all', 'web'], alt: 'AlignVR - Website Design' },
@@ -11,7 +12,7 @@ var app = new Vue({
             { id: 'tasko-parking', format: 'png', tags: ['all', 'graphic', 'branding'], alt: 'Tasko Parking - Logo Design' },
             { id: 'gamification-poster', format: 'jpg', tags: ['all', 'graphic'], alt: 'The Gamification of Jurisprudence research poster' },
             { id: 'bear-beer', format: 'jpg', tags: ['all', 'graphic', 'branding'], alt: 'Bear Beer - Contest Marketing Campaign' },
-            { id: 'world-junior-team1260', format: 'jpg', tags: ['all', 'graphic', 'branding'], alt: '2012 World Junior Championship coverage advertisements' },
+            { id: 'world-junior-team1260', format: 'jpg', tags: ['all', 'graphic', 'branding'], alt: 'World Junior Championship coverage advertisements' },
             { id: 'cars-with-stars', format: 'jpg', tags: ['all', 'graphic', 'branding'], alt: 'Cars with Stars - Contest Marketing Campaign' },
             { id: 'love-ya-party', format: 'jpg', tags: ['all', 'graphic', 'branding'], alt: '"Love Ya!" Party - Event Marketing Campaign' },
             { id: 'make-a-better-city', format: 'jpg', tags: ['all', 'graphic'], alt: 'Make A Better City Classroom Program' }
